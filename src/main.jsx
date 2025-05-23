6700e735d27559c9aec9ee7108bfc942d505d19b
adrianodomingues <adrianodomingues@hotmail.com>
2025-05-23 11:04:56 -0300

    grid

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
